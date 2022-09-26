e52f06712f4ad3d7d7af36263a04094b20a49a1c
Fatih KEŞ <113646979+fatihkes@users.noreply.github.com>
2022-09-26 17:22:11 +0300

    Add files via upload

---
+++ KullanıcıGirisEkranı_/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KullanıcıGirisEkranı_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "fatihkes";
+            string email = "fatihkess0@gmail.com";
+            string password = "123456";
+
+            Console.WriteLine("Kullanıcı / E-mail Giriniz");
+            Console.WriteLine("<<<====================>>>");
+            string name = Console.ReadLine();
+            Console.WriteLine("Parolanızı Giriniz");
+            Console.WriteLine("<<<====================>>>");
+            string sifre = Console.ReadLine();
+            bool durum = (username == name || email == name) && password == sifre;
+            Console.WriteLine(durum);
+            if (durum == false)
+            Console.WriteLine("Giriş Başarısız Tekrar Deneyiniz");
+            else Console.WriteLine("Giriş Başarılı");
+            Console.WriteLine("<<<====================>>>");
+            Console.ReadLine();
+        }
+    }
+}
